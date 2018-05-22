c650736bb2e91670c187f789c10dc04ee6181aef
Jince P George <jince123@gmail.com>
2018-05-22 15:45:32 +0530

    checkin checkout flow done

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,19 @@
 //Import libraries for making Component
 import React, { Component } from 'react';
 import {Text,  StyleSheet,View} from 'react-native';
+import axios from 'axios';
 
 //make a Component
 class AlbumList extends Component{
   componentWillMount(){
     console.log('componet will mount');
+    axios.get('https://rallycoding.herokuapp.com/api/music_album')
+    .then(response=>console.log(response));
   }
   render(){
     return (
       <View>
-           <Text>Album List!!</Text>
+           <Text>Album List</Text>
       </View>
     );
   };
